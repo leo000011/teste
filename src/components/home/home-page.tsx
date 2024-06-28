32d66be47dd60f7c84e799ca29be5dcdf4ce06a7
Leo <leandroano8@gmail.com>
2024-06-28 04:52:49 -0300

    dsadasd

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -3,20 +3,20 @@ import HomeSection from "./home-section";
 import Props from "@/app/type/type";
 
 const HomePage = async () => {
-  const response = await fetch(`${process.env.URL_LINK}/api/products`, {
-    method: "GET",
-  });
-  const data = await response.json();
+  // const response = await fetch(`${process.env.URL_LINK}/api/products`, {
+  //   method: "GET",
+  // });
+  // const data = await response.json();
 
   return (
     <>
       <p>sd</p>
       <div className="flex justify-center items-center h-screen">
-        <ul className="text-white bg-blue-800  ">
+        {/* <ul className="text-white bg-blue-800  ">
           {data.getBank.map((e: Props) => (
             <li key={e._id}>
               {e.description}, {e.title}, {e.price}, {e.imageUrl}
-            </li>
+            </li> */}
           ))}
         </ul>
       </div>
